ab5d733f80bade55b631f4387aa50281e03c5650
Matías Castillo <matcasti@umag.cl>
2023-05-29 18:13:28 -0300

    first commit
    - its rather a shame that I've come this far to remember that I needed to commit before losing everything LOL

---
+++ _site/libs/app.js
@@ -0,0 +1,10 @@
+/* -----------------------------------------------
+/* How to use? : Check the GitHub README
+/* ----------------------------------------------- */
+
+/* To load a config file (particles.json) you need to host this demo (MAMP/WAMP/local)... */
+
+particlesJS.load('particles-js', 'libs/particles.json', function() {
+  console.log('particles.js loaded - callback');
+});
+
